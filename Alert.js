3b7c38872ed5088d2dc287ecf6a231346bafa951
karolwasemann <kw.kwaq@gmail.com>
2022-05-30 14:33:56 +0200

    Finish| added remove & status functions & animation show and remove

--- Alert.js
+++ Alert.js
@@ -1,53 +1,50 @@
 export default class Alert {
-  constructor(type, color, text) {
+  constructor(type, message) {
     this.type = type;
-    this.color = color;
-    this.text = text;
-    this.textContent = `
-    <div
+    this.message = message;
+  }
+  static colors = {
+    Error: 'red',
+    Warning: 'yellow',
+    Success: 'green',
+  };
+
+  getTemplate() {
+    return `<span
       class="
         inline-flex
-        items-center
-        bg-white
-        leading-none
-        text-red-600
+        bg-${Alert.colors[this.type]}-600
+        text-white
         rounded-full
-        p-2
-        shadow
-        text-teal text-sm
+        h-6
+        px-3
+        justify-center
+        items-center
       "
+      >${this.type}</span
     >
-      <span
-        class="
-          inline-flex
-          bg-${color}-600
-          text-white
-          rounded-full
-          h-6
-          px-3
-          justify-center
-          items-center
-        "
-        >${type}</span
-      >
-      <span class="inline-flex px-2">${text}</span>
-    </div>`;
+    <span class="inline-flex px-2">${this.message}</span>`;
   }
 
   display() {
     const alertContainer = document.createElement('div');
     const container = document.querySelector('.container');
-    alertContainer.classList.add('p-2', 'alert', `${this.type}`);
-    alertContainer.innerHTML = this.textContent;
+
+    alertContainer.classList.add(
+      'alert',
+      'm-3',
+      'inline-flex',
+      'items-center',
+      'bg-white',
+      'leading-none',
+      'text-red-600',
+      'rounded-full',
+      'p-2',
+      'shadow',
+      'text-teal',
+      'text-sm'
+    );
+    alertContainer.innerHTML = this.getTemplate();
     container.appendChild(alertContainer);
-    // console.log(alertContainer);
   }
-}
-
-// class Success extends Alert {}
-
-// class Warning extends Alert {}
-
-class Error extends Alert {
-  constructor(type, color, text) {}
 }
--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 // Import stylesheets
 import './style.css';
 import Alert from './Alert.js';
+const container = document.querySelector('.container');
 
-const Error = new Alert('error', 'red');
-const Warning = new Alert('warning', 'yellow');
+const Error = new Alert('Error', 'Coś totalnie poszło nie tak');
 
-Error.display();
-Warning.display();
+const Warning = new Alert('Warning', 'Uważaj! To co robisz, nie jest ok!');
+
+const Success = new Alert('Success', 'Cudownie! Działaj dalej');
+
+const displayAlert = () => {
+  setTimeout(() => {
+    Error.display();
+  }, 700);
+
+  setTimeout(() => {
+    Warning.display();
+  }, 1200);
+
+  setTimeout(() => {
+    Success.display();
+  }, 2000);
+};
+displayAlert();
+
+const removeAlert = () => {
+  container.addEventListener('click', (e) => {
+    if (!e.target.closest('.alert')) return null;
+
+    e.target.closest('.alert').classList.add('remove');
+
+    setTimeout(() => {
+      e.target.closest('.alert').remove();
+      statusAlert();
+    }, 800);
+  });
+};
+
+const statusAlert = () => {
+  const alerts = [Error, Warning, Success];
+  if (container.childElementCount < 3) {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const randomAlert = alerts[randomNumber];
+    setTimeout(() => {
+      randomAlert.display();
+    }, 1200);
+  }
+};
+
+removeAlert();
